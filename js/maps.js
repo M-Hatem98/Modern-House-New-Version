84d77c8b25a8c614e944ba706ea71f1a1282a939
Mohammed Hatem <moh.hat226@gmail.com>
2024-05-27 10:10:44 +0300

    first commit

---
+++ js/maps.js
@@ -0,0 +1,522 @@
+var property = {
+    "data": [
+        {
+            "id": 1,
+            "title": "The Green Restourant",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Restourant",
+            "review": 5,
+            'pull-left': '5 days ago',
+            "rating": 4,
+            "latitude": 51.541599,
+            "longitude": -0.112588,
+            "image": "img/property/img-1.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-cook",
+
+            "address": "123 Kathal St. Tampa City",
+            "area": 2,
+            "bathroom": 3,
+            "garage": 1,
+            'date': '5 days ago',
+            'author': 'Jhon Doe',
+        },
+
+        {
+            "id": 2,
+            "title": "The Hotel Alpha",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Hotel",
+            "review": 8,
+            "rating": 4,
+            "latitude": 51.538395,
+            "longitude": -0.097418,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-2.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-hotel",
+        },
+        {
+            "id": 3,
+            "title": "Gym In Fitnees",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Gym",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.539212,
+            "longitude": -0.118403,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-3.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-gym",
+        },
+        {
+            "id": 4,
+            "title": "My Parsonal Event",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Event",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.522340,
+            "longitude": -0.037894,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-4.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-monitor",
+        },
+        {
+            "id": 6,
+            "title": "Shop In City",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Shop",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.513965,
+            "longitude": -0.038837,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-6.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-shop",
+        },
+        {
+            "id": 7,
+            "title": "The Green Restourant",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Restourant",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.503965,
+            "longitude": -0.058837,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-1.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-cook",
+        },
+        {
+            "id": 8,
+            "title": "The Hotel Alpha",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Hotel",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.555385,
+            "longitude": -0.128274,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-2.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-cook",
+        },
+        {
+            "id": 9,
+            "title": "Gym In Fitnees",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Gym",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.560935,
+            "longitude": -0.111365,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-3.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-gym",
+        },
+        {
+            "id": 10,
+            "title": "My Parsonal Event",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Event",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.543803,
+            "longitude": -0.036607,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-4.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-monitor",
+        },
+        {
+            "id": 10,
+            "title": "Shop In City",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Shop",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.527306,
+            "longitude": -0.140977,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-6.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-shop",
+        },
+        {
+            "id": 11,
+            "title": "Shop In City",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Shop",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.541599,
+            "longitude": -0.112588,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-6.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-shop",
+        },
+        {
+            "id": 12,
+            "title": "The Hotel Alpha",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Hotel",
+            "review": 8,
+            "rating": 4,
+            "latitude": 51.538395,
+            "longitude": -0.097418,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-2.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-hotel",
+        },
+        {
+            "id": 13,
+            "title": "Gym In Fitnees",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Event",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.539212,
+            "longitude": -0.118403,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-3.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-gym",
+        },
+        {
+            "id": 14,
+            "title": "My Parsonal Event",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Event",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.522340,
+            "longitude": -0.037894,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-4.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-monitor",
+        },
+        {
+            "id": 15,
+            "title": "The Green Restourant",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+            "category": "Restourant",
+            "review": 5,
+            "rating": 4,
+            "latitude": 51.531311,
+            "longitude": -0.052314,
+            "address": "123 Kathal St. Tampa City",
+            "email": "info@themevessel.com",
+            "phone": "+0477 85X6 552",
+            "image": "img/property/img-1.jpg",
+            "type_icon": "img/building.png",
+            "icon": "flaticon-cook",
+        },
+    ]
+};
+
+function drawInfoWindow(property) {
+    var image = 'img/logo.png';
+    if (property.image) {
+        image = property.image
+    }
+
+    var title = 'N/A';
+    if (property.title) {
+        title = property.title
+    }
+
+    var address = '';
+    if (property.address) {
+        address = property.address
+    }
+
+    var email = '';
+    if (property.email) {
+        email = property.email
+    }
+
+    var phone = '';
+    if (property.phone) {
+        phone = property.phone
+    }
+
+    var ibContent = '';
+    ibContent =
+        "<div class='map-property'>" +
+        "<div class='map-img'>" +
+        "<img src='" + image + "'/><div class=\"price-ratings-box\">\n" +
+
+        "                                </div>" +
+        "</div>" +
+        "<div class='map-content'>" +
+        "<h4><a href='single-property.html'>" + title + "</a></h4>" +
+        "<p class='address'> <i class='flaticon-pin'></i>" + address + "</p>" +
+        "<div class='map-property-fetures'> " +
+        "</div>" +
+        "</div>";
+    return ibContent;
+}
+
+function insertPropertyToArray(property, layout) {
+    var image = 'img/logo.png';
+    if (property.image) {
+        image = property.image
+    }
+
+    var address = '';
+    if (property.address) {
+        address = property.address
+    }
+
+    var email = '';
+    if (property.email) {
+        email = property.email
+    }
+
+    var phone = '';
+    if (property.phone) {
+        phone = property.phone
+    }
+
+    var title = 'N/A';
+    if (property.title) {
+        title = property.title
+    }
+
+    var category = '<div class="tag">SALE</div>';
+    if (property.category) {
+        category = '<div class="tag">'+property.category+'</div>'
+    }
+
+    var desc = '';
+    if (property.desc) {
+        desc = property.desc;
+    }
+
+    var review = '';
+    if (property.review) {
+        review = property.review;
+    }
+
+    var rating = '';
+    if (property.rating) {
+        rating = property.rating;
+    }
+
+    var icon = '';
+    if (property.icon) {
+        icon = property.icon;
+    }
+
+    var element = '';
+
+    if(layout == 'grid_layout'){
+        element = '<div class="col-lg-6 col-md-6 col-sm-12" ><div class="property-box">' +
+            '<div class="property-thumbnail">            ' +
+            '   <div class="listing-photo">            ' +
+            '       <div class="now-open open-color">For Sale</div>            ' +
+                    category +
+            '       <a class="icon">            ' +
+            '           <i class="flaticon-heart"></i> ' +
+            '       </a>            ' +
+            '       <img class="d-block w-100" src="'+image+'" alt="listing-photo"> ' +
+            '   </div>' +
+            '</div>' +
+            '<div class="detail">            ' +
+            '   <h3 class="title">            ' +
+            '       <a href="single-property.html">'+title+'</a> ' +
+            '   </h3>            ' +
+            '   <ul>            ' +
+            '       <li><i class="flaticon-pin"></i>'+address+'</li>            ' +
+            '       <li><i class="flaticon-phone"></i>'+phone+'</li>            ' +
+            '       <li><i class="flaticon-mail"></i>'+email+'</li> ' +
+            '   </ul>' +
+            '</div>' +
+            '<div class="footer">            ' +
+            '<div class="ratings">            ' +
+            '   <i class="fa fa-star"></i>            ' +
+            '   <i class="fa fa-star"></i>            ' +
+            '   <i class="fa fa-star"></i>            ' +
+            '   <i class="fa fa-star"></i>            ' +
+            '   <i class="fa fa-star-o"></i>            ' +
+            '   <span>('+rating+' Reviews)</span>            ' +
+            '</div>' +
+            '</div>' +
+            '</div></div>';
+    }
+    else{
+        element = '<div class="property-box-2"> ' +
+            '<div class="row">             ' +
+            '   <div class="col-lg-5 col-md-5 col-pad"> ' +
+            '       <div class="property-thumbnail">             ' +
+        '               <a href="single-property.html" class="listing-img">             ' +
+        '                   <img src="'+image+'" alt="listing-photo-2" class="img-fluid"> ' +
+                           category +
+        '                   <div class="icon"> ' +
+        '                       <i class="flaticon-heart"></i> ' +
+        '                   </div>             ' +
+            '               <div class="now-open featured-color">Featured</div> ' +
+            '           </a> ' +
+            '       </div> ' +
+            '   </div>             ' +
+            '   <div class="col-lg-7 col-md-7 col-pad">             ' +
+            '       <div class="detail">             ' +
+            '           <h3 class="title">             ' +
+            '               <a href="single-property.html">'+title+'</a> ' +
+            '           </h3>            ' +
+            '           <ul>            ' +
+            '               <li><i class="flaticon-pin"></i>'+address+'</li> ' +
+            '               <li><i class="flaticon-phone"></i>'+phone+'</li>            ' +
+            '               <li><i class="flaticon-mail"></i>'+email+'</li> ' +
+            '           </ul> ' +
+            '       </div>            ' +
+            '       <div class="footer">            ' +
+                '       <div class="ratings pull-left">            ' +
+                '           <i class="fa fa-star"></i> ' +
+                '           <i class="fa fa-star"></i> ' +
+                '           <i class="fa fa-star"></i> ' +
+                '           <i class="fa fa-star"></i>            ' +
+                '           <i class="fa fa-star-o"></i>           ' +
+                '           <span>('+rating+' Reviews)</span>            ' +
+                '       </div>            ' +
+                '   <div class="pull-right open-now">        ' +
+            '           Open Now            ' +
+            '       </div>            ' +
+        '       </div>            ' +
+            '</div>' +
+            '</div> ' +
+            '</div>'
+    }
+    return element;
+}
+
+function animatedMarkers(map, propertyMarkers, property, layout) {
+    var bounds = map.getBounds();
+    var propertyArray = [];
+    $.each(propertyMarkers, function (key, value) {
+        if (bounds.contains(propertyMarkers[key].getLatLng())) {
+            propertyArray.push(insertPropertyToArray(property.data[key], layout));
+            setTimeout(function () {
+                if (propertyMarkers[key]._icon != null) {
+                    propertyMarkers[key]._icon.className = 'leaflet-marker-icon leaflet-zoom-animated leaflet-clickable bounce-animation marker-loaded';
+                }
+            }, key * 50);
+        }
+        else {
+            if (propertyMarkers[key]._icon != null) {
+                propertyMarkers[key]._icon.className = 'leaflet-marker-icon leaflet-zoom-animated leaflet-clickable';
+            }
+        }
+    });
+    $('.fetching-property').html(propertyArray);
+}
+
+function generateMap(latitude, longitude, mapProvider, layout) {
+
+    var map = L.map('map', {
+        center: [latitude, longitude],
+        zoom: 14,
+        scrollWheelZoom: false
+    });
+
+    L.tileLayer.provider(mapProvider).addTo(map);
+    var markers = L.markerClusterGroup({
+        showCoverageOnHover: false,
+        zoomToBoundsOnClick: false
+    });
+    var propertyMarkers = [];
+
+    $.each(property.data, function (index, property) {
+        var icon = '<img src="img/logos/black-logo.png">';
+        if (property.type_icon) {
+            icon = '<img src="' + property.type_icon + '">';
+        }
+        var color = '';
+        var markerContent =
+            '<div class="map-marker ' + color + '">' +
+            '<div class="icon">' +
+            icon +
+            '</div>' +
+            '</div>';
+
+        var _icon = L.divIcon({
+            html: markerContent,
+            iconSize: [36, 46],
+            iconAnchor: [18, 32],
+            popupAnchor: [130, -28],
+            className: ''
+        });
+
+        var marker = L.marker(new L.LatLng(property.latitude, property.longitude), {
+            title: property.title,
+            icon: _icon
+        });
+
+        propertyMarkers.push(marker);
+        marker.bindPopup(drawInfoWindow(property));
+        markers.addLayer(marker);
+        marker.on('popupopen', function () {
+            this._icon.className += ' marker-active';
+        });
+        marker.on('popupclose', function () {
+            this._icon.className = 'leaflet-marker-icon leaflet-zoom-animated leaflet-clickable marker-loaded';
+        });
+    });
+
+    map.addLayer(markers);
+    animatedMarkers(map, propertyMarkers, property, layout);
+    map.on('moveend', function () {
+        animatedMarkers(map, propertyMarkers, property, layout);
+    });
+
+    $('.fetching-property .property-box-2, .fetching-property .property-box').hover(
+        function () {
+            propertyMarkers[$(this).attr('id') - 1]._icon.className = 'leaflet-marker-icon leaflet-zoom-animated leaflet-clickable marker-loaded marker-active';
+        },
+        function () {
+            propertyMarkers[$(this).attr('id') - 1]._icon.className = 'leaflet-marker-icon leaflet-zoom-animated leaflet-clickable marker-loaded';
+        }
+    );
+
+
+
+    $('.geolocation').on("click", function () {
+        map.locate({setView: true})
+    });
+    $('#map').removeClass('fade-map');
+}
